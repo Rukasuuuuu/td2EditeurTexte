0ee9b873a47b16f59f6937f42af3eaaddb17148f
Lucas Vrignaud <lucas.vrignaud@etu.umontpellier.fr>
2023-11-09 11:58:10 +0100

    ajout commande remplacer

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,23 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+public class CommandeRemplacer extends CommandeDocument {
+
+    public CommandeRemplacer(Document document, String[] parameters) {super(document, parameters);}
+
+    @Override
+    public void executer() {
+        if(parameters.length < 2) {
+            System.err.println("Format attendu : remplacer;debut;fin");
+            return;
+        }
+        String debut = parameters[1];
+        String fin = parameters[2];
+        String texte="";
+        if ( parameters.length > 3){
+            texte = parameters[3];
+        }
+        this.document.remplacer(Integer.parseInt(debut), Integer.parseInt(fin), texte);
+        super.executer();
+    }
+}
