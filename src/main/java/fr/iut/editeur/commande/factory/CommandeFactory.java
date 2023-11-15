d7f4899fd675bb96c8a6f5ab50b01d89f36de766
Lucas Vrignaud <lucas.vrignaud@etu.umontpellier.fr>
2023-11-15 14:46:39 +0100

    test gitHUB

--- src/main/java/fr/iut/editeur/commande/factory/CommandeFactory.java
+++ src/main/java/fr/iut/editeur/commande/factory/CommandeFactory.java
@@ -24,6 +24,7 @@ public class CommandeFactory {
             case "effacer" : return new CommandeEffacer(document, parameters);
             case "clear" : return new CommandeClear(document, parameters);
 
+
             default: return null;
         }
     }
