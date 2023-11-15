e9111891cc0bb69fb6abc519af01258c1b1bae29
Lucas Vrignaud <lucas.vrignaud@etu.umontpellier.fr>
2023-11-15 15:06:46 +0100

    push

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -27,23 +27,45 @@ public class Document {
         return this.texte;
     }
 
+
+    /**
+     * remplace le texte entre deux index en parametres par le mot definis en parametre ( remplacement)
+     * @param start
+     * @param end
+     * @param remplacement
+     */
     public void remplacer(int start, int end, String remplacement) {
         String leftPart = texte.substring(0, start);
         String rightPart = texte.substring(end);
         texte = leftPart + remplacement + rightPart;
     }
 
+
+    /**
+     * met en majsucule la partie de texte compris entre les deux index en parametres
+     * @param start
+     * @param end
+     */
     public void majuscules(int start, int end) {
         String s = texte.substring(start, end);
         String sModif = s.toUpperCase();
         remplacer(start, end, sModif);
     }
 
+    /**
+     * efface la partie de texte compris entre les deux index en parametre
+     * @param start
+     * @param end
+     */
     public void effacer(int start, int end){
         String sModif = "";
         remplacer(start, end, sModif);
     }
 
+
+    /**
+     * reinitialise le document, le document devient vide
+     */
     public void clear(){
         String sModif = "";
         remplacer(0, texte.length(), sModif );
